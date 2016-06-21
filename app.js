06fd1c240e261d1c87ee772abb5e430f62d2f6e1
Samwise12 <sfelder1@bingahmton.edu>
2016-06-21 18:44:52 -0400

    test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 var express = require('express');
 var app = express();
 
@@ -7,7 +8,10 @@ app.get('/', function(req,res){
 		language: null,
 		software: null
 	};
-	format.ipaddress = req.headers['x-forward-for']|| req.connection.remoteAddress;	
+	format.ipaddress = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
 	format.language = req.headers["accept-language"].split(',')[0];
 	format.software = 	req.headers["user-agent"].match(/\((.*?)\)/)[1];
 	res.json(format);
